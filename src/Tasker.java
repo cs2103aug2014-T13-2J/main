0e63462e66b3c6c14c3d058156cf523fa482b2a6
Nee Si Yao <neesiyao@gmail.com>
2014-10-02 00:41:50 +0800

    Add code to print welcome message

--- src/Tasker.java
+++ src/Tasker.java
@@ -6,13 +6,15 @@ import java.util.Scanner;
 
 public class Tasker {
 	
+	public static final String MESSAGE_WELCOME = "Welcome to Tasker!";
+	
 	public static void main(String[] args) {
 		initializeEnvironment();
 		readAndExecuteCommands();
 	}
 	
 	public static void initializeEnvironment() {
-		
+		System.out.println(MESSAGE_WELCOME);
 	}
 	
 	public static void readAndExecuteCommands() {
