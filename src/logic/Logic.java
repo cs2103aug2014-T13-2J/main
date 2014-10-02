cded73c5cfeed7eba81a92dbdaadb858b0a1161d
mervynwee <mail.wyrm@gmail.com>
2014-10-02 23:51:10 +0800

    Start defining function addTask under Logic class

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -1,13 +1,34 @@
 package logic;
+
+import java.util.ArrayList;
+
 public class Logic {
 	
 	public static String addTask(String details) {
-		//This function allows the user to enter commands of the following formats:
-		//"add meeting on next Thursday at 11pm"
-		//"add meeting on next Thursday from 11pm to 1am"
-		//"add gathering on 3/10/2014"
-		//"add gathering on 3/10"
-		//"add gathering on 3 October"
+		String description, venue, startDate, endDate, startTime;
+		
+		ArrayList<Integer> atList = new ArrayList<Integer>();
+		ArrayList<Integer> onList = new ArrayList<Integer>();
+		ArrayList<Integer> nextList = new ArrayList<Integer>();
+		
+		String[] words = details.split(" ");
+		
+		String currentWord;
+		//run through every word and look for "on" or "at" or "next" and store their positions
+		for(int i = 0; i < words.length; i++) {
+			currentWord = words[i].toLowerCase();
+			if(currentWord.equals("on")) {
+				onList.add(i);
+			}
+			else if(currentWord.equals("at")) {
+				atList.add(i);
+			}
+			else if(currentWord.equals("next")) {
+				nextList.add(i);
+			}
+		}
+		
+		
 		return null;
 	}
 
