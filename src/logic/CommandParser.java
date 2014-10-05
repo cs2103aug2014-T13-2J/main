e42a334358c86ec42a7c3bc0affa16d27058652c
Nee Si Yao <neesiyao@gmail.com>
2014-10-05 17:49:13 +0800

    Add functionality to getCommandType and getCommandDetails methods

--- src/logic/CommandParser.java
+++ src/logic/CommandParser.java
@@ -4,11 +4,13 @@ public abstract class CommandParser {
 	
 	public abstract String parse(String userInput);
 	
-	protected String getCmdType(String userInput) {
-		return null;
+	protected String getCommandType(String userInput) {
+		String commandType = userInput.trim().split(" ")[0];
+		return commandType;
 	}
 	
-	protected String getCmdDetails(String userInput) {
-		return null;
+	protected String getCommandDetails(String userInput) {
+		String commandDetails = userInput.trim().split(" ", 2)[1];
+		return commandDetails;
 	}
 }
