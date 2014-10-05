055f36f1e2be1c3ca2003741c5a521874f29bc0c
Michelle Shwe <shwemichelle@gmail.com>
2014-10-05 15:42:39 +0800

    Defined functions in Tasker class

--- src/ui/Tasker.java
+++ src/ui/Tasker.java
@@ -1,5 +1,12 @@
 package ui;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+
 
 public class Tasker {
 	
@@ -10,16 +17,41 @@ public class Tasker {
 		readAndExecuteCommands();
 	}
 	
+	
 	public static void initializeEnvironment() {
 		System.out.println(MESSAGE_WELCOME);
+		filename=args[0];
+		initializeFileForIO(filename);
 	}
 	
-	public static void readAndExecuteCommands() {
-		
+	public static void readAndExecuteCommands() throws IOException{
+		scanner = new Scanner(System.in);
+		while(true){
+			System.out.println(MESSAGE_PROMPT);
+			String userCommand=scanner.nextLine();
+			Interpreter.interpretCmd(userCommand);
+		}
 	}
 	
-	public static String readCmd(Scanner sc) {
+	/*public static String readCmd(Scanner sc) {
 		return null;
+	}*/
+	
+	public static void initializeFileForIO(String userArgument){
+		File file = new File(userArgument);
+		if (!file.exists()) {
+			file.createNewFile();
+			}
+			FileReader fileReader = new FileReader(filename); 
+			BufferedReader reader = new BufferedReader(fileReader); 
+			String textLine;
+			textArray = new ArrayList<>();
+			while ((textLine = reader.readLine()) != null) {
+			textArray.add(addedText);
+			}
+			reader.close(); 
+			fileReader.close();
 	}
+	
 
 }
