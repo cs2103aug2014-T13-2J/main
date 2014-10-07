79aa8228f73f4f26eb526b744acbafec4bd2c7ba
Michelle Shwe <shwemichelle@gmail.com>
2014-10-07 18:28:09 +0800

    Made changes to Tasker class in

--- src/main/ui/Tasker.java
+++ src/main/ui/Tasker.java
@@ -3,23 +3,19 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
 import java.io.BufferedReader;
 import java.io.IOException;
 
 import main.logic.CommandParser;
 
 public class Tasker {
+	public static String filename;
 	
 	public static final String MESSAGE_WELCOME = "Welcome to Tasker!";
 	public static final String MESSAGE_PROMPT = "Enter Command:";
-	public static String[] args = null;
-	public static String filename = args[0];
-
-	public static void initializeEnvironment() throws IOException {
+	
+	public static void initializeEnvironment() {
 		System.out.println(MESSAGE_WELCOME);
-		initializeFileForIO(filename);
 	}
 	
 	public static void readAndExecuteCommands() throws IOException{
@@ -36,7 +32,8 @@ public class Tasker {
 	}*/
 	
 	public static void initializeFileForIO(String userArgument) throws IOException{
-		File file = new File(userArgument);
+		filename = userArgument;
+		File file = new File(filename);
 		if (!file.exists()) {
 			file.createNewFile();
 			}
@@ -50,6 +47,7 @@ public class Tasker {
 			reader.close(); 
 			fileReader.close();
 	}
+
 	
 
 }
