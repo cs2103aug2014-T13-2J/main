bc4564f2f95207866cdea2e9e3c573f3d2624a6f
Michelle Shwe <shwemichelle@gmail.com>
2014-10-08 20:27:30 +0800

    Slight changes to SearchParser
    Still unsure of where to search from though.

--- src/main/logic/SearchParser.java
+++ src/main/logic/SearchParser.java
@@ -8,29 +8,33 @@ public class SearchParser extends CommandParser {
 		return null;
 	}
 
-	public static <ArrayList>String searchTask(String searchString) {
-		ArrayList<String> linesWithTheKey = new ArrayList<String>();
-		ArrayList<String> copyOfEntries = new ArrayList<String>();
-		String lowerCaseKey = "";
-		
-		if(list.isEmpty()){
-			return null;
-		}
-		lowerCaseKey = string.toLowerCase();
-		
-		for(int i = 0; i < list.size(); i++){
-			copyOfEntries.add(list.get(i));
-		}
-		
-		for(int i = 0; i < list.size(); i++){
-			if(copyOfEntries.get(i).toLowerCase().contains(lowerCaseKey)){
-				linesWithTheKey.add(list.get(i));
+	public String parser(String userInput) {
+		 ArrayList<String> linesWithTheKey = new ArrayList<String>();
+		 ArrayList<String> copyOfEntries = new ArrayList<String>(); 
+		 ArrayList<String> list = new ArrayList<String>();
+
+		 String lowerCaseKey = "";
+		 
+			if(list.isEmpty()){ 
+				return null;
 			}
+			
+			lowerCaseKey = userInput.toLowerCase();
+			
+			for(int i = 0; i < list.size(); i++){ 
+				copyOfEntries.add(list.get(i));
+			}
+			for(int i = 0; i < list.size(); i++){ 
+				if(copyOfEntries.get(i).toLowerCase().contains(lowerCaseKey)){
+					linesWithTheKey.add(list.get(i));
+				} 
+			}
+			for (int i=0; i < linesWithTheKey.size(); i++) { 
+				System.out.println((i+1) + "." + linesWithTheKey.get(i));
+			}
+			
+			return null;
+			
 		}
-		
-		for (int i=0; i < linesWithTheKey.size(); i++) {
-			System.out.println((i+1) + "." + linesWithTheKey.get(i));
-		}
-		return linesWithTheKey;
+
 	}
-}
