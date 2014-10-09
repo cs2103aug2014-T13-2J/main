faea7c8190cb07c4241c28af167ad3dffa8f0d15
Nee Si Yao <neesiyao@gmail.com>
2014-10-09 09:39:33 +0800

    Complete DeleteHandler class.

--- src/main/logic/DeleteHandler.java
+++ src/main/logic/DeleteHandler.java
@@ -1,15 +1,26 @@
 package main.logic;
 
+import java.util.ArrayList;
+
+import main.storage.Task;
+
 public class DeleteHandler extends CommandHandler {
+	
+	private String details;
 
 	public DeleteHandler(String details) {
 		super(details);
-		// TODO Auto-generated constructor stub
+		details = this.details;
 	}
 
 	@Override
 	public String execute() {
-		return null;
+		ArrayList<Task> tasks = getCurrentTaskList();
+		int userIndex = Integer.valueOf(details);
+		int arrayListIndex = userIndex + 1;
+		tasks.remove(arrayListIndex);
+		String message = "Task " + userIndex + " has been deleted.";
+		return message;
 	}
 
 }
