d780cc37562e0e2bc6c85aeb13fc3457b5bd7e57
Nee Si Yao <neesiyao@gmail.com>
2014-10-09 09:08:22 +0800

    Create DisplayHandler class.

---
+++ src/main/logic/DisplayHandler.java
@@ -0,0 +1,26 @@
+package main.logic;
+
+import java.util.List;
+
+import main.storage.Task;
+
+public class DisplayHandler extends CommandHandler {
+
+	public DisplayHandler(String details) {
+		super(details);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public String execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	protected List<Task> getCurrentTaskList() {
+		// TODO Auto-generated method stub
+		return super.getCurrentTaskList();
+	}
+
+}
