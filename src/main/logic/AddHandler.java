700b164dd26c6adcf06e495ce2bd7d6cef556c57
Nee Si Yao <neesiyao@gmail.com>
2014-10-11 17:45:54 +0800

    Add TaskBuilder instantiation and startDateTime, endDateTime instantiation for AddHandler execute function in order to create a task.

--- src/main/logic/AddHandler.java
+++ src/main/logic/AddHandler.java
@@ -1,5 +1,7 @@
 package main.logic;
 
+import main.storage.TaskBuilder;
+
 public class AddHandler extends CommandHandler {
 	
 	private AddParser parser;
@@ -20,6 +22,14 @@ public class AddHandler extends CommandHandler {
 		}
 		
 		//use TaskBuilder to create the task
+		TaskBuilder builder = new TaskBuilder();
+		builder.setDescription(parser.getDescription());
+		builder.setVenue(parser.getVenue());
+		
+		DateTime startDateTime = new DateTime(parser.getStartDateYear(), parser.getStartDateMonth(), parser.getStartDateDay, parser.getStartTimeHour(), 
+				parser.getStartTimeMinute(), DEFAULT_SECOND, DEFAULT_MILLISECOND);
+		DateTime startDateTime = new DateTime(parser.getEndDateYear(), parser.getEndDateMonth(), parser.getEndDateDay, parser.getEndTimeHour(), 
+				parser.getEndTimeMinute(), DEFAULT_SECOND, DEFAULT_MILLISECOND);
 		//store the task in storage
 		
 		return null;
