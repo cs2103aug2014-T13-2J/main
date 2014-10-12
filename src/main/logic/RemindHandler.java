5c47eddabd60381f6ad93673c77c711f55e6987c
Nee Si Yao <neesiyao@gmail.com>
2014-10-12 11:16:46 +0800

    Create RemindHandler and RemindParser for setting reminders.

---
+++ src/main/logic/RemindHandler.java
@@ -0,0 +1,16 @@
+package main.logic;
+
+public class RemindHandler extends CommandHandler {
+
+	public RemindHandler(String details) {
+		super(details);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
