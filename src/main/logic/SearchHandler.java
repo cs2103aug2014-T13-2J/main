70c4ac9b2e7e58e360824ede52487336c41bc32b
Michelle Shwe <shwemichelle@gmail.com>
2014-10-14 13:17:46 +0800

    modified SearchHandler and SearchParser to implement Search function.

--- src/main/logic/SearchHandler.java
+++ src/main/logic/SearchHandler.java
@@ -1,15 +1,22 @@
 package main.logic;
 
+import java.util.ArrayList;
+
+import main.storage.Task;
+
 public class SearchHandler extends CommandHandler {
+
+	private SearchParser parser;
 
 	public SearchHandler(String details) {
 		super(details);
-		// TODO Auto-generated constructor stub
+		ArrayList<Task> tasks = getCurrentTaskList();
+		parser = new SearchParser(details, tasks);
 	}
 
 	@Override
 	public String execute() {
-		return null;
+		return parser.parse();
 	}
 
 }
--- src/main/logic/SearchParser.java
+++ src/main/logic/SearchParser.java
@@ -1,46 +1,53 @@
 package main.logic;
+
 import java.util.ArrayList;
 
+import main.storage.Task;
+import main.logic.DisplayHandler;
+
 public class SearchParser extends CommandParser {
-	
-	public SearchParser(String arguments) {
+	public static final String MESSAGE_ERROR = "No search term specified.";
+	public static final String MESSAGE_NULL = "There is nothing to search.";
+	private String userInput;
+	private ArrayList<Task> list;
+
+	public SearchParser(String arguments, ArrayList<Task> tasks) {
 		super(arguments);
-		// TODO Auto-generated constructor stub
+		userInput = arguments;
+		list = tasks;
+
 	}
 
 	@Override
 	public String parse() {
-		// search should be done here
-		return null;
-	}
+		ArrayList<Task> linesWithTheKey = new ArrayList<Task>();
+		String returnMessage = "";
+		String lowerCaseKey = "";
 
-	public String parser(String userInput) {
-		 ArrayList<String> linesWithTheKey = new ArrayList<String>();
-		 ArrayList<String> copyOfEntries = new ArrayList<String>(); 
-		 ArrayList<String> list = new ArrayList<String>();
+		if (list.isEmpty()) {
+			return MESSAGE_NULL;
+		}
 
-		 String lowerCaseKey = "";
-		 
-			if(list.isEmpty()){ 
-				return null;
-			}
-			
+		else if (userInput.equals("")) {
+			return MESSAGE_ERROR;
+		}
+
+		else {
 			lowerCaseKey = userInput.toLowerCase();
-			
-			for(int i = 0; i < list.size(); i++){ 
-				copyOfEntries.add(list.get(i));
-			}
-			for(int i = 0; i < list.size(); i++){ 
-				if(copyOfEntries.get(i).toLowerCase().contains(lowerCaseKey)){
+
+			for (int i = 0; i < list.size(); i++) {
+				if (list.get(i).toString().toLowerCase().contains(lowerCaseKey)) {
 					linesWithTheKey.add(list.get(i));
-				} 
+				}
 			}
-			for (int i=0; i < linesWithTheKey.size(); i++) { 
-				System.out.println((i+1) + "." + linesWithTheKey.get(i));
+
+			for (int i = 0; i < linesWithTheKey.size(); i++) {
+				DisplayHandler.displayTask(i, linesWithTheKey.get(i));
 			}
-			
-			return null;
-			
+
+			return returnMessage;
+
 		}
 
 	}
+}
