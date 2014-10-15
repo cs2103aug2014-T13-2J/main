004951a076942453f0190f3ce2fa5a4675bda3f9
mervynwee <mail.wyrm@gmail.com>
2014-10-15 19:58:22 +0800

    Reimplement UpdateHandler class. Remove all the code within execute function. Remove String details attribute and add UpdateParser attribute. Modify constructor to initialize these attributes accordingly. Start implementing UpdateParser class.

--- src/main/logic/UpdateHandler.java
+++ src/main/logic/UpdateHandler.java
@@ -1,51 +1,42 @@
 package main.logic;
 
-import java.util.ArrayList;
-
-import org.joda.time.DateTime;
-
-import main.storage.Task;
-
 public class UpdateHandler extends CommandHandler {
 	
-	private String details;
+	private UpdateParser parser;
 
 	public UpdateHandler(String details) {
 		super(details);
-		details = this.details;
+		parser = new UpdateParser(details);
 	}
 
 	@Override
 	public String execute() {
-		int index = getUserIndex(details) - 1;
-		ArrayList<Task> tasks = getCurrentTaskList();
-		Task currentTask = tasks.get(index);
-		String taskParameter = getSecondWord(details);
-		switch (taskParameter.toLowerCase()) {
-		case "time":
-			String newTime = getThirdWord(details);
-			DateTime newDateTime;
-			currentTask.setStartDateTime(newDateTime);
-			String message = "Time has been changed to " + newTime; 
-			return message;
-		default:
-			return null;	
+		try {
+			//parser will analyze the user input and store each piece of information into its respective
+			//attributes
+			parser.parse();
+		} catch (IllegalArgumentException e) {
+			return e.getMessage();
 		}
+		String field = parser.getField();
+		if(field.equals(UpdateParser.FIELD_DESCRIPTION)) {
+			//get task
+			//update task
+		} else if (field.equals(UpdateParser.FIELD_START_DATE)) {
+			
+		} else if (field.equals(UpdateParser.FIELD_START_TIME)) {
+			
+		} else if (field.equals(UpdateParser.FIELD_END_DATE)) {
+			
+		} else if (field.equals(UpdateParser.FIELD_END_TIME)) {
+			
+		} else if (field.equals(UpdateParser.FIELD_RECURRENCE)) {
+			
+		} else if (field.equals(UpdateParser.FIELD_REMINDER)) {
+			
+		} else { //FIELD_COMPLETED
+			
+		}
+		return null;
 	}
-	
-	private int getUserIndex(String details) {
-		int userIndex = Integer.valueOf(details.trim().split(" ")[0]);
-		return userIndex;
-	}
-	
-	private String getSecondWord(String details) {
-		String secondWord = details.trim().split(" ")[1];
-		return secondWord;
-	}
-	
-	private String getThirdWord(String details) {
-		String thirdWord = details.trim().split(" ")[2];
-		return thirdWord;
-	}
-
 }
--- src/main/logic/UpdateParser.java
+++ src/main/logic/UpdateParser.java
@@ -1,15 +1,181 @@
 package main.logic;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class UpdateParser extends CommandParser {
 
+	private Integer taskNumber;
+	private String field;
+
+	protected final static String FIELD_DESCRIPTION = "description";
+	protected final static String FIELD_START = "start";
+	protected final static String FIELD_START_DATE = "startDate";
+	protected final static String FIELD_START_TIME = "startTime";
+	protected final static String FIELD_END = "end";
+	protected final static String FIELD_END_DATE = "endDate";
+	protected final static String FIELD_END_TIME = "endTime";
+	protected final static String FIELD_RECURRENCE = "recurrence";
+	protected final static String FIELD_REMINDER = "reminder";
+	protected final static String FIELD_COMPLETED = "completed";
+	private final static Integer INDEX_FIRST_WORD = 0;
+	private final static String MESSAGE_INVALID_FIELD = "Sorry we did not understand which field you were trying to update.";
+	private final static String MESSAGE_INVALID_TASK_NUMBER = "Task number is not a valid number.";
+
+
+	/***************************** Constructors ************************/
 	public UpdateParser(String arguments) {
 		super(arguments);
-		// TODO Auto-generated constructor stub
+	}
+
+	/***************************** Accessors ************************/
+
+	public Integer getTaskNumber() {
+		return taskNumber;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	/***************************** Mutators ************************/
+
+	public void setTaskNumber(String number) throws IllegalArgumentException {
+		Integer result;
+		result = convertStringToInteger(number);
+		this.taskNumber = result;
+	}
+	
+	public void setField(String field) {
+		if(field.equals(FIELD_DESCRIPTION) || field.equals(FIELD_START_DATE) || field.equals(FIELD_START_TIME)
+				|| field.equals(FIELD_END_DATE) || field.equals(FIELD_END_TIME) || field.equals(FIELD_RECURRENCE)
+				|| field.equals(FIELD_REMINDER) || field.equals(FIELD_COMPLETED)) {
+			this.field = field;
+		} else {
+			throw new IllegalArgumentException(MESSAGE_INVALID_FIELD);
+		}
+	}
+
+	private boolean isValidField(String field) {
+		if (field.equals(FIELD_DESCRIPTION) || field.equals(FIELD_START)
+				|| field.equals(FIELD_END)
+				|| field.equals(FIELD_RECURRENCE)
+				|| field.equals(FIELD_REMINDER)
+				|| field.equals(FIELD_COMPLETED)) {
+			return true;
+		} else {
+			throw new IllegalArgumentException(MESSAGE_INVALID_FIELD);
+		}
+	}
+
+	private void setFieldAndDetails(String field, String details, LinkedList<String> wordsList) {
+		if (field.equals(UpdateParser.FIELD_DESCRIPTION)) {
+			this.setDescription(details);
+			this.setField(FIELD_DESCRIPTION);
+		} else if (field.equals(UpdateParser.FIELD_START)) {
+			if(representsTime(details)) {
+				String startTime = getTime(wordsList);
+				setStartTime(startTime);
+				this.setField(FIELD_START_TIME);
+			} else {
+				String startDate = getDateAndTrimUserInput(wordsList);
+				setStartDate(startDate);
+				this.setField(FIELD_START_DATE);
+			}
+		} else if (field.equals(UpdateParser.FIELD_END)) {
+			if(representsTime(details)) {
+				String endTime = getTime(wordsList);
+				setEndTime(endTime);
+				this.setField(FIELD_END_TIME);
+			} else {
+				String endDate = getDateAndTrimUserInput(wordsList);
+				setEndDate(endDate);
+				this.setField(FIELD_END_DATE);
+			}		
+		} else if (field.equals(UpdateParser.FIELD_RECURRENCE)) {
+			setRecurrence(details);
+			this.setField(FIELD_RECURRENCE);
+		} else if (field.equals(UpdateParser.FIELD_REMINDER)) {
+			setReminder(details);
+			this.setField(FIELD_REMINDER);
+		} else { // FIELD_COMPLETED
+			setCompleted(details);
+			this.setField(FIELD_COMPLETED);
+		}
 	}
 
 	@Override
 	public String parse() {
-		return null;
+		String[] userInput = this.getUserInput().split(" ");
+		LinkedList<String> wordsList = new LinkedList<String>(
+				Arrays.asList(userInput));
+
+		String taskNumber = wordsList.get(INDEX_FIRST_WORD);
+		setTaskNumber(taskNumber);
+		wordsList.poll();
+
+		String field = wordsList.get(INDEX_FIRST_WORD);
+		if(isValidField(field)){
+			wordsList.poll(); //remove field from user input
+			String details = getDetails(wordsList);
+			setFieldAndDetails(field, details, wordsList);
+		}
+		
+
+		
+		
+
+		return MESSAGE_PARSE_SUCCESS;
 	}
 	
+	private static String getDetails(LinkedList<String> wordsList) {
+		String details = "";
+		while (!wordsList.isEmpty()) {
+			details += wordsList.poll();
+		}
+		return details;
+	}
+	
+	private static Integer convertStringToInteger(String number) {
+		try {
+			Integer result = Integer.parseInt(number);
+			return result;
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(MESSAGE_INVALID_TASK_NUMBER);
+		}
+	}
+	
+	private void setStartDate(String startDate) {
+		String startDateYear = getYear(startDate);
+		String startDateMonth = getMonth(startDate);
+		String startDateDay = getDay(startDate);
+		
+		this.setStartDateYear(startDateYear);
+		this.setStartDateMonth(startDateMonth);
+		this.setStartDateDay(startDateDay);
+	}
+	
+	private void setEndDate(String endDate) {
+		String endDateYear = getYear(endDate);
+		String endDateMonth = getMonth(endDate);
+		String endDateDay = getDay(endDate);
+		
+		this.setEndDateYear(endDateYear);
+		this.setEndDateMonth(endDateMonth);
+		this.setEndDateDay(endDateDay);
+	}
+	
+	private void setStartTime(String startTime) {
+		String startTimeHour = getHour(startTime);
+		String startTimeMinute = getMinute(startTime);
+		this.setStartTimeHour(startTimeHour);
+		this.setStartTimeMinute(startTimeMinute);
+	}
+	
+	private void setEndTime(String endTime) {
+		String endTimeHour = getHour(endTime);
+		String endTimeMinute = getMinute(endTime);
+		this.setEndTimeHour(endTimeHour);
+		this.setEndTimeMinute(endTimeMinute);
+	}
 }
