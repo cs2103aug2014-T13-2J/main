b6a2dfce1f83404476dc77b4f3060dd1c333c879
Nee Si Yao <neesiyao@gmail.com>
2014-10-22 20:24:18 +0800

    Add function to sync adding of non-floating task to Google Calendar.

--- src/main/googlecalendar/GoogleCalendar.java
+++ src/main/googlecalendar/GoogleCalendar.java
@@ -4,8 +4,16 @@ import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.ParseException;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.TimeZone;
+
+import main.storage.Task;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
@@ -13,12 +21,18 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventDateTime;
 
 public class GoogleCalendar {
 	
+	private static String MESSAGE_PRE_LOGIN = "Logging in to Google Calendar...\n\n"
+			+ "Tasker will now open your default browser and direct you to a page for an authorisation code.\n\n"
+			+ "Copy the authorisation code and paste it back here once you are done!\n\n"
+			+ "Press ENTER to continue...";
 	private static String NAME_APPLICATION = "Tasker";
 	private static String MESSAGE_LOGIN_SUCCESS = "You have successfully logged in to Google Calendar!";
 	private static String MESSAGE_SYNC_SUCCESS = "Task successfully synchronised!";
@@ -46,12 +60,7 @@ public class GoogleCalendar {
 
 		String url = flow.newAuthorizationUrl().setRedirectUri(redirectUrl)
 				.build();
-		System.out.println("Logging in to Google Calendar...\n");
-		System.out
-				.println("Tasker will now open your default browser and direct you to a page for an authorisation code.\n");
-		System.out
-				.println("Copy the authorisation code and paste it back here once you are done!");
-		System.out.println("Press ENTER to continue...");
+		System.out.println(MESSAGE_PRE_LOGIN);
 		Scanner scanner = new Scanner(System.in);
 		scanner.nextLine();
 
@@ -74,8 +83,34 @@ public class GoogleCalendar {
 		return MESSAGE_LOGIN_SUCCESS;
 	}
 	
-	public static String syncTask(Event event) throws IOException {
-		service.events().insert("primary", event);
+	public static String syncAddNonFloatingTask(Event event) throws IOException {
+		service.events().insert("primary", event).execute();
 		return MESSAGE_SYNC_SUCCESS;
+	}
+	
+	public static Event convertNonFloatingTaskToEvent(Task task) throws ParseException {
+		Event event = new Event();
+		event.setSummary(task.getDescription());
+		event.setLocation(task.getVenue());
+		
+		LocalDate taskStartDate = task.getStartDate();
+		LocalTime taskStartTime = task.getStartTime();
+		org.joda.time.DateTime jodaStartDateTime = taskStartDate.toDateTime(taskStartTime);
+		Date startDate = jodaStartDateTime.toDate();
+		DateTime startDateTime = new DateTime(startDate, TimeZone.getTimeZone("UTC"));
+		EventDateTime eventStartDateTime = new EventDateTime();
+		eventStartDateTime.setDateTime(startDateTime);
+		event.setStart(eventStartDateTime);
+		
+		LocalDate taskEndDate = task.getEndDate();
+		LocalTime taskEndTime = task.getEndTime();
+		org.joda.time.DateTime jodaEndDateTime = taskEndDate.toDateTime(taskEndTime);
+		Date endDate = jodaEndDateTime.toDate();
+		DateTime endDateTime = new DateTime(endDate, TimeZone.getTimeZone("UTC"));
+		EventDateTime eventEndDateTime = new EventDateTime();
+		eventEndDateTime.setDateTime(endDateTime);
+		event.setEnd(eventEndDateTime);
+		
+		return event;
 	}
 }
