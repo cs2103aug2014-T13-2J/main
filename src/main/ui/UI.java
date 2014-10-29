dd186e9b4c52f05a11c3fcf3fc7447f4a6426e63
Michelle Shwe <shwemichelle@gmail.com>
2014-10-30 00:36:34 +0800

    Removed Jline from UI class (again). Formatted UITest class.

--- src/main/ui/UI.java
+++ src/main/ui/UI.java
@@ -1,18 +1,20 @@
 package main.ui;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import org.fusesource.jansi.AnsiConsole;
 
 import main.TaskerLog;
 import main.googlecalendar.GoogleCalendar;
+import main.logic.Logic;
 import main.storage.Storage;
 
 public class UI {
 	public static String filename;
 
 	public static final String MESSAGE_WELCOME = "Welcome to Tasker!";
-	private static TabCompletion tab = new TabCompletion();
+	public static final String MESSAGE_PROMPT = "Enter command:";
 	private static GoogleCalendar googleCalendar = GoogleCalendar.getInstance();
 
 	public static void initializeEnvironment() {
@@ -24,7 +26,14 @@ public class UI {
 	}
 
 	public static void readAndExecuteCommands() throws IOException {
-		tab.run();
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println(MESSAGE_PROMPT);
+			String userCommand = scanner.nextLine();
+			TaskerLog.logSystemInfo("User entered:" + userCommand);
+			System.out.println(Logic.uiToLogic(userCommand));
+		}
 	}
 
 }
--- src/test/ui/UITest.java
+++ src/test/ui/UITest.java
@@ -28,58 +28,56 @@ public class UITest {
 		Storage storage = Storage.getInstance();
 		ArrayList<Task> tasks = storage.getTasks();
 
-		//Test the UI input for basic CRUD functions
-		
-		//adding without any description
+		// Test the UI input for basic CRUD functions
+
+		// adding without any description
 		String dataOne = "add";
 		Logic.uiToLogic(dataOne);
 		String expectedOne = "Sorry, we did not capture your description. Please try again.";
-		assertEquals(expectedOne,Logic.uiToLogic(dataOne));
-		
+		assertEquals(expectedOne, Logic.uiToLogic(dataOne));
+
 		tasks.clear();
-		//add task with description, venue, date and time
-		String dataTwo ="add meeting at clb at 9pm next tuesday";
+		// add task with description, venue, date and time
+		String dataTwo = "add meeting at clb at 9pm next tuesday";
 		Logic.uiToLogic(dataTwo);
 		String expectedTwo = "meeting at clb on 2014-11-04 at 21:00 added!";
-		assertEquals(expectedTwo,Logic.uiToLogic(dataTwo));
-		
-		//display task
-		String dataThree ="display";
+		assertEquals(expectedTwo, Logic.uiToLogic(dataTwo));
+
+		// display task
+		String dataThree = "display";
 		Logic.uiToLogic(dataThree);
-		String result ="";
-		result = String.format(DisplayHandler.DISPLAY_NUM_OF_TASKS, tasks.size());
+		String result = "";
+		result = String.format(DisplayHandler.DISPLAY_NUM_OF_TASKS,
+				tasks.size());
 
 		result += String.format(DisplayHandler.DISPLAY_TABLE_ROW_STRING_FORMAT,
 				ansi().fg(RED).a("ID").reset(),
-				ansi().fg(MAGENTA).a(" DESCRIPTION").reset(),
-				ansi().fg(CYAN).a(" VENUE").reset(),
-				ansi().fg(YELLOW).a(" TIME").reset(),
-				ansi().fg(GREEN).a(" DATE").reset());
+				ansi().fg(MAGENTA).a(" DESCRIPTION").reset(), ansi().fg(CYAN)
+						.a(" VENUE").reset(), ansi().fg(YELLOW).a(" TIME")
+						.reset(), ansi().fg(GREEN).a(" DATE").reset());
 		result += DisplayHandler.displayLineSeparator();
-		for(int i=0;i<tasks.size();i++){
+		for (int i = 0; i < tasks.size(); i++) {
 			result += DisplayHandler.displayTaskInTable(i, tasks.get(i));
 		}
 		result += DisplayHandler.displayLineSeparator();
 		result += String.format(DisplayHandler.DISPLAY_TABLE_ROW_STRING_FORMAT,
 				ansi().fg(RED).a("ID").reset(),
-				ansi().fg(MAGENTA).a(" DESCRIPTION").reset(),
-				ansi().fg(CYAN).a(" VENUE").reset(),
-				ansi().fg(YELLOW).a(" TIME").reset(),
-				ansi().fg(GREEN).a(" DATE").reset());		
-		assertEquals(result,Logic.uiToLogic(dataThree));
-	
-	
-	//update task venue
-	String dataFour = "update 1 venue nlb";
-	String expectedFour = "Task 1's venue updated!";
-	Logic.uiToLogic(dataFour);
-	assertEquals(expectedFour, Logic.uiToLogic(dataFour));
-	
-	//delete tasks in an empty list
-	String dataFive = "delete all";
-	String expectedFive = "Sorry, the arguments must only contain the task IDs to be deleted. Please try again.";
-	Logic.uiToLogic(dataFive);
-	assertEquals(expectedFive, Logic.uiToLogic(dataFive));
-	
+				ansi().fg(MAGENTA).a(" DESCRIPTION").reset(), ansi().fg(CYAN)
+						.a(" VENUE").reset(), ansi().fg(YELLOW).a(" TIME")
+						.reset(), ansi().fg(GREEN).a(" DATE").reset());
+		assertEquals(result, Logic.uiToLogic(dataThree));
+
+		// update task venue
+		String dataFour = "update 1 venue nlb";
+		String expectedFour = "Task 1's venue updated!";
+		Logic.uiToLogic(dataFour);
+		assertEquals(expectedFour, Logic.uiToLogic(dataFour));
+
+		// delete tasks in an empty list
+		String dataFive = "delete all";
+		String expectedFive = "Sorry, the arguments must only contain the task IDs to be deleted. Please try again.";
+		Logic.uiToLogic(dataFive);
+		assertEquals(expectedFive, Logic.uiToLogic(dataFive));
+
 	}
 }
