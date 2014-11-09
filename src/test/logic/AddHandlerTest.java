a155b615fad06785b325c2c068036fd4ab79b030
mervynwee <mail.wyrm@gmail.com>
2014-11-10 00:53:58 +0800

    Create new test cases to catch exceptions thrown.

--- src/test/logic/AddHandlerTest.java
+++ src/test/logic/AddHandlerTest.java
@@ -36,7 +36,7 @@ public class AddHandlerTest {
 		
 		String s2 = "meeting with Prof at CLB at 11pm on 3/10";
 		CommandHandler e2 = new AddHandler(s2);
-		assertEquals("Success message: ", "meeting with Prof at CLB on 2014-10-03 at 23:00 added!", e2.execute());
+		assertEquals("Success message: ", "meeting with Prof at CLB on 2015-10-03 at 23:00 added!", e2.execute());
 		
 		String s3 = "meeting with Prof at CLB on 4/12 at 11pm";
 		CommandHandler e3 = new AddHandler(s3);
@@ -48,11 +48,11 @@ public class AddHandlerTest {
 		
 		String s5 = "gathering on 3/10";
 		CommandHandler e5 = new AddHandler(s5);
-		assertEquals("Success message: ", "gathering on 2014-10-03 added!", e5.execute());
+		assertEquals("Success message: ", "gathering on 2015-10-03 added!", e5.execute());
 
 		String s6 = "gathering on 3 October";	
 		CommandHandler e6 = new AddHandler(s6);
-		assertEquals("Success message: ", "gathering on 2014-10-03 added!", e6.execute());
+		assertEquals("Success message: ", "gathering on 2015-10-03 added!", e6.execute());
 		
 		String s7 = "gathering on 3 October 2014";
 		CommandHandler e7 = new AddHandler(s7);
@@ -64,15 +64,15 @@ public class AddHandlerTest {
 		
 		String s9 = "meeting from 3/10 to 4/10 at Utown";
 		CommandHandler e9 = new AddHandler(s9);
-		assertEquals("Success message: ", "meeting at Utown from 2014-10-03 to 2014-10-04 added!", e9.execute());
+		assertEquals("Success message: ", "meeting at Utown from 2015-10-03 to 2015-10-04 added!", e9.execute());
 		
 		String s10 = "meeting from 3 October 11pm to 4 October 1am at Utown";
 		CommandHandler e10 = new AddHandler(s10);
-		assertEquals("Success message: ", "meeting at Utown from 2014-10-03 23:00 to 2014-10-04 01:00 added!", e10.execute());
+		assertEquals("Success message: ", "meeting at Utown from 2015-10-03 23:00 to 2015-10-04 01:00 added!", e10.execute());
 		
 		String s11 = "meeting from 3/10 to 18/10";
 		CommandHandler e11 = new AddHandler(s11);
-		assertEquals("Success message: ", "meeting from 2014-10-03 to 2014-10-18 added!", e11.execute());
+		assertEquals("Success message: ", "meeting from 2015-10-03 to 2015-10-18 added!", e11.execute());
 		
 		String s12 = "meeting with Prof on 4/12 at CLB at 11pm";
 		CommandHandler e12 = new AddHandler(s12);
@@ -82,10 +82,16 @@ public class AddHandlerTest {
 		CommandHandler e13 = new AddHandler(s13);
 		assertEquals("Success message: ", "meeting with Prof at CLB on 2014-12-04 at 23:00 added!", e13.execute());
 	}
-
+	
 	@Test
-	public void testConvertParsedDetailsToTask() {
-		fail("Not yet implemented");
+	public void testFromToInvalidTime() throws Exception {
+		String s1 = "task from 11/10 2 pm to 12/10 4pm";
+		CommandHandler e13 = new AddHandler(s1);
 	}
-
+	
+	@Test
+	public void testFromToInvalidDate() throws Exception {
+		String s1 = "task from 11//10 2 pm to 12/10 4pm";
+		CommandHandler e1 = new AddHandler(s1);
+	}
 }
